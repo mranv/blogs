06fe61c46b01e10f36ca399cbdd6a89240b491df
anubhavg-icpl <anubhavg@infopercept.com>
2025-08-03 21:47:15 +0530

    refactor codebase

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-// lib/utils.ts
-import clsx from "clsx"
-import type { ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
