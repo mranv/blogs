243fe1298658f4b5637e3f8919c41f726c00f7bc
anubhavg-icpl <anubhavg@infopercept.com>
2025-08-03 22:13:08 +0530

    update the codbease for binary channel.

--- src/config.ts
+++ src/config.ts
@@ -11,10 +11,6 @@ export const siteConfig: SiteConfig = {
 	title: "Fuwari",
 	subtitle: "Demo Site",
 	lang: "en", // Language code, e.g. 'en', 'zh-CN', 'ja', etc.
-	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
-	},
 	banner: {
 		enable: false,
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
